ee6e43534f1f85a3f235294381aa86daea5f3f3e
Hung Chin <a0975322508@gmail.com>
2021-11-30 15:37:02 +0800

    java Math

--- Gitfinsh/src/Gitfinsh/Hello.java
+++ Gitfinsh/src/Gitfinsh/Hello.java
@@ -1,7 +1,10 @@
 package Gitfinsh;
 public class Hello {
 	public static void main(String[] args) {
-		System.out.print("Hello! 你好!請問有中文介面");
+		System.out.print("Hello! 你好!請問有中文介面嗎?");
+		
 	}
+	
+	
 
 }
---
+++ Gitfinsh/src/Gitfinsh/Math.java
@@ -0,0 +1,21 @@
+package Gitfinsh;
+
+public class Math {
+	
+	public static void main(String[] args) {
+		Math1 cMath = new Math1();
+		System.out.print(cMath.add(2, 5));
+	}
+	
+	
+	
+	
+	
+}
+class Math1{
+	int add(int a , int b) {
+	 return	a + b;
+	}
+}
+
+
